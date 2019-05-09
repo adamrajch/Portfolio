8622610a9a45f48b7da2f46a24da4b0279ca5d3b
Golden Dragon <arajchwald@gmail.com>
2019-05-09 14:36:27 -0500

    Updates

---
+++ precache-manifest.95d76f7d9353b5f0a0064d4f93d8f60d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acfe3b648f8a1f6cf5a2583dcae95ed3",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "4e3376a24965d69e2c25",
+    "url": "/repository-name/static/css/main.550e5cf5.chunk.css"
+  },
+  {
+    "revision": "cdfac9ab91eb98234640",
+    "url": "/repository-name/static/js/2.ded533d9.chunk.js"
+  },
+  {
+    "revision": "4e3376a24965d69e2c25",
+    "url": "/repository-name/static/js/main.741bd43a.chunk.js"
+  },
+  {
+    "revision": "6e2fc4b7d77a9c53df47",
+    "url": "/repository-name/static/js/runtime~main.f7634681.js"
+  }
+]);
